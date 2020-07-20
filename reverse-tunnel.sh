be251844c1841c17d8fed008e7840f1a4a7560d6
mchen6 <seeds.c@gmail.com>
2020-07-20 14:42:11 +0800

    customized modification

--- reverse-tunnel.sh
+++ reverse-tunnel.sh
@@ -7,6 +7,10 @@
 # behind a NAT in the local network.
 # Disconnections and re-connections will be gracefully handled by autossh. For this
 # to work you should setup automatic authentication using SSH keys.
+# 1. First run ssh-keygen on local machine and save to .ssh/nopwd.pub
+# 2. Then run ssh-copy-id -i .ssh/nopwd.pub -p 22 root@REMOTE_HOST
+# 3. Then run this script with nohup: nohup ./reverse-tunnel.sh &  Or add it to /etc/rc.local script
+#
 #
 # It does the following:
 # 1) Establishes an SSH connection to REMOTE_HOST;
@@ -28,21 +32,27 @@
 # Last revised 17/10/2019
 
 # The remote host that we connect to via SSH, and establish the listening remote port(s)
-REMOTE_HOST=remotehost.example.net
+REMOTE_HOST=101.37.82.72
 REMOTE_HOST_SSH_PORT=22
-REMOTE_HOST_SSH_USER=myusername
+REMOTE_HOST_SSH_USER=root
 
 
 # Define reverse port forwards
 # Format: 'REMOTE_PORT:LOCAL_HOST:LOCAL_PORT' (where LOCAL_HOST can be actual localhost or any host acessible by localhost)
 PORTS=(
-     "8880:localhost:80"    # 8880 -> 80
-     "8443:localhost:443"   # 8443 -> 443
+     "2222:192.168.0.80:22"    # 8880 -> 80
+     "3030:192.168.0.80:3030"   # 8443 -> 443
+     "3300:192.168.0.80:3300"   # 8443 -> 443
+     "9527:192.168.0.80:9527"   # 8443 -> 443
+     "61000:192.168.0.80:61000"   # 8443 -> 443
+     "61001:192.168.0.80:61001"   # 8443 -> 443
+     "61002:192.168.0.80:61002"   # 8443 -> 443
+     "61003:192.168.0.80:61003"   # 8443 -> 443
     )
 
 
 for PORT in ${PORTS[@]}
-do   
+do
   PORT_STR="$PORT_STR -R 0.0.0.0:$PORT"
 done
 
@@ -50,4 +60,5 @@ done
 # Ignore early failed connections at boot
 export AUTOSSH_GATETIME=0
 
-autossh -4 -M 0 -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes $PORT_STR -p$REMOTE_HOST_SSH_PORT $REMOTE_HOST_SSH_USER@$REMOTE_HOST
+autossh -4 -M 0 -N -o PubkeyAuthentication=yes -o PasswordAuthentication=no -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -i /root/.ssh/nopwd $PORT_STR -p$REMOTE_HOST_SSH_PORT $REMOTE_HOST_SSH_USER@$REMOTE_HOST
+
